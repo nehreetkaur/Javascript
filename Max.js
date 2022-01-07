7f913f1716b2706e639284f8a33b4b818ffa079a
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-01-07 11:27:04 -0600

    Max.js complted

---
+++ Max.js
@@ -0,0 +1,17 @@
+const result= function(num1,num2){
+ return Math.max(num1,num2)
+}
+
+//console.log(result(2,10));
+
+
+// Another way
+
+let num1=42;
+let num2=12;
+
+if(num1 > num2){
+    console.log("Max num is:" + num1)
+}else{
+    console.log("Max num is:" + num2)
+}
