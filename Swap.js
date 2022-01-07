4406100835f8b559b8b6a28073df7f53691506ed
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-01-07 11:16:09 -0600

    file completed

---
+++ Swap.js
@@ -0,0 +1,13 @@
+// to swap two values
+
+let value1= 2;
+let value2= 4;
+
+let swappedVar =value1;
+console.log("Value of swappedVar is" + swappedVar)
+value1=value2;
+console.log("value of value1 :" + value1)
+value2=swappedVar;
+
+console.log("Value1 is:" +value1)
+console.log("Value2 is:" +value2)
