3fa5115a7d7de340c6cd8f5819e258b44c3a4340
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-02-14 12:23:37 -0600

    Objects started

---
+++ changekeys.js
@@ -0,0 +1,24 @@
+// let person={
+// names: "spiderman",
+// names: "Thor",
+// names :"Ironman",
+// names :"wonderwoman"
+// }
+
+
+let person = {
+    firstName: 'Peter',
+    lastName: 'Doe',
+    employeeId: 1
+};
+
+// display of object
+console.log(person);
+
+// adding new properties
+console.log(Object.keys(person));
+console.log(Object.entries(person));
+
+
+// if we want to delete it
+// delete person.lastName;
