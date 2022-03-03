81d13ec0a4d184437959269251cdddbf015a9d3b
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-03 17:35:57 -0600

    string charat function code solved

--- stringCheck.js
+++ stringCheck.js
@@ -10,7 +10,7 @@ function checking(str){
     }
 }
 
-console.log(checking("I am enough"));
+//console.log(checking("I am enough"));
 
 // to check wheather a string is blank or not
 
@@ -23,5 +23,67 @@ function checking2(str){
     }
 }
 
-console.log(checking2(""));
+//console.log(checking2(""));
+
+// split string in to array of words;
+
+function checking3(str){
+    return str.split(" ");    // little space between them converted words in to array
+}  
+                            // no space between them converted in to individual word in to array.
+console.log(checking3("Shiva Pandya"))
+
+// to truncate the string
+
+function trimming(str,value){
+return str.slice(0,value)
+}
+
+//console.log(trimming("Robin Singh",4))
+
+
+// to return the string in abbrevated form
+
+function  checking4(str){
+    const strArr = str.split(' ');
+   let res = '';
+   strArr.forEach(el => {
+      const [char] = el;
+      //console.log(el.charAt(0))
+      //console.log(el)
+      if(char === char.toUpperCase() && char !== char.toLowerCase()){
+         res += char;
+      };
+   });
+   return res;
+};
+
+
+//console.log(checking4("Shiva Ravi Pandya"));
+
+
+// let str= "hello there";
+// let length= str.split(" ");
+// console.log(length[0])
+// length.forEach((el)=>{
+// let ss= el.charAt(0).toUpperCase();
+// console.log(ss)
+// })
+// like if we split the string["shiva","pandya"]
+//and when we loop through with forEach and
+// then use charAt(0) will give us back S and p
+// which is on 0th index of both the strings and then we 
+// do our calculation how we like;
+
+function stringCheck(str){
+let result= str.split(" ");
+let final=[];
+result.forEach((e)=>{
+  console.log(e.charAt(0));
+  final.push(e.charAt(0))
+})
+return `${result[0]}  ${final.pop()}`. toUpperCase();
+}
+
+console.log(stringCheck("Robin Singh"));
 
