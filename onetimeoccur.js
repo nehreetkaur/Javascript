95de1b9dfe676b7c5f85963af44cd33c6c29fde1
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-08 18:28:45 -0600

    repeated string tested in several cases completed

--- onetimeoccur.js
+++ onetimeoccur.js
@@ -10,8 +10,8 @@ function checking(str){
       count[item]=1
   }
   })
-//  
-// let final;
+  
+
 let final= Object.keys(count).find((keys)=>{
     return count[keys]===1;
 })
@@ -25,7 +25,7 @@ return final;
 
 
 
-//console.log(checking("hehlloz"))
+console.log(checking("the qquk brown fox jump over the moonz"))  // to tell the character which is not repeated
 
 
 // second approach
@@ -39,7 +39,28 @@ function checking1(str){
     return "_"
 }
 
-console.log(checking1("hehlloz"));
+//console.log(checking1("hehlloz"));
 
 
-console.log("hello".lastIndexOf("e"))
+// I am passing a string to look that particular is present or not;
+
+
+let string="hello there people hello";
+let find="pretty";
+let count=0;
+let result=string.split(" ");
+//console.log(result);
+for(let i=0;i<result.length;i++){
+    //console.log(result[i])
+    if(result[i]===find){
+        //console.log("hurray")
+        count+=1;
+        
+    }
+   
+}
+
+console.log(`${find} is repeated that many ${count} times`)
+
+
+
--- truncateString.js
+++ truncateString.js
@@ -8,3 +8,41 @@ function checking(str,num){
 }
 
 console.log(checking("Helloozz",3))
+
+// to chop a string in to given strength ["w3resource"] into ["w3", "re", "so", "ur", "ce"]
+
+
+
+function returnChunksArray(str, chunkSize) {
+    var arr = [];
+    while(str !== '') {
+      arr.push(str.slice(0, chunkSize));
+      console.log(arr);
+      str = str.slice(chunkSize);
+      console.log(str)
+    }
+    return arr;
+  }
+
+
+
+
+
+
+
+
+  console.log( returnChunksArray("w3resource",2))
+
+
+  //console.log("w3resource".slice(2))
+
+//   console.log("hello".lastIndexOf("e"))
+//   console.log("hello".indexOf("e"))
+
+
+
+
+
+
+
+
