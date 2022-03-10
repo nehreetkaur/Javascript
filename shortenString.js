c9b911e63ad00d802351772d131320cf7bd1a611
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-10 16:57:07 -0600

    string code

---
+++ shortenString.js
@@ -0,0 +1,27 @@
+function checking(str,num){
+
+return str.split(" ").slice(0,num).join(" ")
+    
+}
+
+//console.log(checking("The quick brown fox jumps over the lazy dog",4))
+
+// function truncate(str, no_words) {
+//     return str.split(" ").splice(0,no_words).join(" ");
+// }
+
+// console.log(truncate('The quick brown fox jumps over the lazy dog', 4));
+
+//number of times the occurence of string;
+// function checking(str,word){
+//     let count=0;
+//     str=str.split(" ");
+//     str.forEach((e)=>{
+//       if(e===word){
+//           count+=1;
+//       }
+//     })
+//     return `${word} is repeated ${count} many times.`
+// }
+
+// console.log(checking("The quick brown fox jumps over the lazy dog","the"));
