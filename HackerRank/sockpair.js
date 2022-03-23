cab064575dd65fc27abf3ffe22fa0362ddbc89ca
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-23 18:44:38 -0500

    comment added

--- HackerRank/sockpair.js
+++ HackerRank/sockpair.js
@@ -19,3 +19,21 @@ console.log(checking(9,[10, 20, 20, 10, 10, 30, 50, 10, 20]))
 
 // if you have found a pair and then delete it and then proceed;
 // one pair = 2 numbers;
+
+
+// function checking(string,path){
+//     let level=0;
+//     let count=0;
+//     string=string.split('');
+//     for(i=0;i<string.length;i++){
+//         if(string[i]==='U'){
+//             level+=1;
+//             if(level==0){
+//                 count+=1;
+//             }
+//         }else{
+//             level-=1;
+//         }
+//     }
+//     return count;
+// }
