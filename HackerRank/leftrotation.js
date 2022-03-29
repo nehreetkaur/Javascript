c77a6256bf5907e1af500832f29756c0166fa06d
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-29 14:16:28 -0500

    leftrotation.js completed

--- HackerRank/leftrotation.js
+++ HackerRank/leftrotation.js
@@ -1,7 +1,33 @@
-function rotation(arr,d){
-let len=arr.length;
-return len;
+function rotation(a,d){
+
+// let p=arr.slice(d);
+// console.log("p is" + p);
+// console.log("Arr is" + arr)
+// let c= arr.slice(0,d);
+// console.log("C is " + c);
+
+for(let i=0;i<d;i++){
+    console.log(i);  // brute force method
+    let m=a.shift()
+    //console.log(m);
+    a.push(m)
+    console.log (a)
 }
 
 
-console.log(rotation([1,2,3,4,5]))
+}
+
+
+
+console.log(rotation([1,2,3,4,5],4))
+
+// function rotLeft(a, d) {
+//     return  [...a.slice(d), ...a.slice(0,d)]
+// }
+
+
+
+// let result = arr.slice(d).concat(arr.slice(0,d)) 
+// return result 
+
+
