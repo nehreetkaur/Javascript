515d9f5c3f201210ca88e542a40555b9c4d5973b
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-04-05 17:50:46 -0500

    make changes in minimumswaps.js

--- HackerRank/minimumswaps.js
+++ HackerRank/minimumswaps.js
@@ -1,24 +1,24 @@
-// function countswaps(arr){
-//     let count=0;
-//     // iterate the array
+function countswaps(arr){
+    let count=0;
+    // iterate the array
 
-//     for(let i=0;i<arr.length;i++){
-//         if(arr[i] !==(i+1)){
-//          let currIndex= arr.indexOf(i+1);
-//          let temp=arr[i];
-//          arr[i]=arr[currIndex];
-//          arr[currIndex]=temp;
-//          count++;
-//         }
-//     }
-//     return `${count}`;
-// }
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] !==(i+1)){
+         let currIndex= arr.indexOf(i+1);
+         let temp=arr[i];
+         arr[i]=arr[currIndex];
+         arr[currIndex]=temp;
+         count++;
+        }
+    }
+    return `${count}`;
+}
 
-// let numbers=[7,1,3,2,4,5,6]
-//console.log(countswaps(numbers));
+ let numbers=[7,1,3,2,4,5,6]
+console.log(countswaps(numbers));
 
 // optimized solution
-function countswaps(arr){
+function countswaps1(arr){
     let count=0;
 
     for(let i=0;i<arr.length;i++){
@@ -36,5 +36,5 @@ function countswaps(arr){
       
 
 }
-let numbers=[4,3,1,2];
-console.log(countswaps(numbers));
+let number=[4,3,1,2];
+console.log(countswaps1(numbers));
