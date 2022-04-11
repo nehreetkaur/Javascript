dfa48c96d5ffcb219cb30e382c0b201f8492c081
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-04-11 13:57:19 -0500

    array manipulation js file created

---
+++ HackerRank/arraymanupulationhac.js
@@ -0,0 +1,5 @@
+// n=5 , we will create array n+1 , so in iteration it will not go out of bound;
+// b=2 we put b+1 which is 3 negative k vlaue to stop and put value till b.
+
+
+//for example if a=2 b=3 we will stop at index 3 as array is here 1 indexed
