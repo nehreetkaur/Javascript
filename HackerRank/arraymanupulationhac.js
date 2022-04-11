34f69ce1af5247af188bf1f241ff84e8a9135608
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-04-11 14:12:05 -0500

    logic building started

--- HackerRank/arraymanupulationhac.js
+++ HackerRank/arraymanupulationhac.js
@@ -3,3 +3,21 @@
 
 
 //for example if a=2 b=3 we will stop at index 3 as array is here 1 indexed
+
+
+function manipulation(n, queries) {
+    let arr = new Array(n + 1).fill(0);
+    queries.forEach(([a, b, k]) => {
+        arr[a - 1] += k;
+        arr[b] -= k;
+    })
+    let sum = 0;
+    let max = 0;
+    arr.forEach(val => {
+        sum += val;
+        max = Math.max(sum, max)
+    })
+    return max;
+}
+
+console.log(manipulation(12,[[2,7,4],[5,9,2],[6,12,8]]))
