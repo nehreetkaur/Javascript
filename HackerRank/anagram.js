9c38854da29dd6b533cc3b66438cc8fc823c9795
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-04-12 17:32:59 -0500

    code commit

--- HackerRank/anagram.js
+++ HackerRank/anagram.js
@@ -2,35 +2,30 @@
 
 function checking (a,b){
 
-    var a = a.split("");
-    var b = b.split("");
-var countA = a.length;
-var countB = b.length;
-var count = countA + countB;
-console.log(count)
-    var pair = 0;
-    for(var i = 0; i < countA;i++){
-        
-        for(var j = 0; j < countB; j++){
-       
-            if(a[i] == b[j]){
-            
-               //console.log(`value of a[i] is ${a[i]}`)
-               pair++;
-               console.log(`b.replace(${j},'')`)
+let A= a.split('').length;
+let B= b.split('').length;
 
-               
-              
-               
-                break;
-            }
+let count= A+B;
+let pairs=0;
+
+for(let i=0;i<A;i++){
+    for(let j=0;j<B;j++){
+        if(a[i]==b[j]){
+            pairs++;
+            b[j]=0;
+           
+            break;
+
         }
     }
-    console.log(count - (pair*2));
+}
+
+console.log(count-(pairs*2))
+
 }
     
 
-checking("hellott","ehlol");
+//checking("hellottw","ehlol");
 
 
 
@@ -42,3 +37,47 @@ checking("hellott","ehlol");
 // for(let i=0;i<string1.length;i++)
 
 // console.log(string1.includes(string2[i]))
+
+// unction main() {
+//     var a = readLine().split("");
+//     var b = readLine().split("");
+// var countA = a.length;
+// var countB = b.length;
+// var count = countA + countB;
+//     var pair = 0;
+//     for(var i = 0; i < countA;i++){
+//         for(var j = 0; j < countB; j++){
+//             if(a[i] == b[j]){
+//                 b.splice(j,1);
+//                 pair++;
+//                 break;
+//             }
+//         }
+//     }
+//     console.log(count - (pair*2));
+// }
+
+
+function makeAnagram(a, b) {
+    const aAr = a.split('');
+    const bAr = b.split('');
+    
+    let removed = 0;
+    
+    for(let i = 0; i < aAr.length ; i++){
+        if(bAr.includes(aAr[i])){
+            bAr.splice(bAr.indexOf(aAr[i]), 1)
+            console.log(bAr);
+        }else{
+            removed++;
+            //console.log(removed);
+            
+        }
+    }
+    
+   
+    return (removed + bAr.length)
+
+}
+
+console.log(makeAnagram("helloq","ehlolwtt"));
