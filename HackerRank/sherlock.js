0de40865c8bf296f4ab2203367f4863444e6d0de
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-04-21 12:26:59 -0500

    sherlock and valid string solved

--- HackerRank/sherlock.js
+++ HackerRank/sherlock.js
@@ -3,10 +3,56 @@
 // s:abccc
 
 
+function checking(s){
+    let obj={};
+    let freq=[]
+    // convert string in to array;
+s=s.split('');
+console.log(s);
+// getting the frequencies
+s.forEach((item)=>{
+     if(obj[item]){
+         obj[item]+=1;
+     }
+     else{
+         obj[item]=1
+     }
+})
+console.log(obj);
+// push the freq in the freq array
+for(let val in obj){
+    freq.push(obj[val])
+}
+
+freq=freq.sort();
+let first= freq[0];
+//console.log(first);
+let sec= freq[1];
+let seclast=freq[freq.length-2];
+//console.log(seclast)
+let last=freq[freq.length-1];
+//console.log(last)
+if(first===last){
+    return 'YES'
+}
+
+if(first==1 && sec==last ){
+    return 'YES'
+}
+
+if(first==sec && sec==seclast && seclast==(last-1)){
+return 'YES'
+}
+else{
+    return 'NO'
+}
+
+}
 
 
 
 
 
 
-console.log(checking("aaaabbbcc"));
+
+console.log(checking("aabbccd"));
