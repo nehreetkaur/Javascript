9d1dda0b3af88040b05ff4a5c3675f69071517c3
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-04-21 11:57:35 -0500

    code commit

--- HackerRank/sherlock.js
+++ HackerRank/sherlock.js
@@ -2,28 +2,11 @@
 
 // s:abccc
 
-function checking(s){
-    let count={};
-    let total=0;
-    s=s.split('');
-    console.log(s)
-    s.forEach((item)=>{
-      if(count[item]){
-          count[item]+=1
-      }
-      else{
-          count[item]=1;
-      }
-    })
-    console.log( count)
-   total= Object.keys(count).map((item)=>{
-      if (count[item]===1 || count[item]===2){
-          return true;
-      }
-            return false;
-  })
-  return total;
 
-}
+
+
+
+
+
 
 console.log(checking("aaaabbbcc"));
